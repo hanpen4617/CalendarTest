9ec4464e5e7c2772aaa14a0ed5a1743a37d9ca54
korou <korou7946@gmail.com>
2022-09-28 22:26:43 +0900

    commit

--- app/src/main/java/com/example/calendartest/CalendarAdapter.kt
+++ app/src/main/java/com/example/calendartest/CalendarAdapter.kt
@@ -10,7 +10,9 @@ import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
 class CalendarAdapter(context: Context) :BaseAdapter(){
+    //カレンダー制作クラスのインスタンス化
     val mDateManager:DateManager = DateManager()
+    //カレンダーデータが入ったリスト
     val dateArray:List<Date> = mDateManager.getCalendar()
     val mLayoutInflater: LayoutInflater = LayoutInflater.from(context)
     class ViewHolder(){
@@ -30,14 +32,19 @@ class CalendarAdapter(context: Context) :BaseAdapter(){
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-        val holder: ViewHolder
-           var convert = convertView
-        if (convert == null){
-            convert = mLayoutInflater.inflate(R.layout.calendar_cell,null)
-            holder = ViewHolder()
+        var holder= ViewHolder()
+        var convert = convertView
+        if(convert == null){
+            //convertに一セル分のデータデザインを格納
+            convert = mLayoutInflater.inflate(R.layout.calendar_cell,parent,false)
+            //holderの中に格納
             holder.text = convert.findViewById(R.id.dateText)
-
+            convert.tag = holder
+        } else {
+            holder = convert.tag as ViewHolder
         }
+        //ここから具体的なデータを入れる処理
+
         return convert
     }
 
